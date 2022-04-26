1120bbf54231b1426aeaecfb4e0757930d1b1441
DesCritas <nik1234nik@gmail.com>
2022-04-26 15:34:47 +0300

    added files and autotests

--- src/main/kotlin/hw211/Main.kt
+++ src/main/kotlin/hw211/Main.kt
@@ -1,2 +1,34 @@
 package hw211
 
+fun main(){
+    val transAmount = 1100000
+    val comission : Int = transaction(transAmount, "Maestro", 6500000)
+    if (comission == -1) {
+        println("Неверный тип карты")
+    } else {
+        println("Перевод $transAmount коп. Комиссия составила: $comission коп.")
+    }
+
+}
+
+fun transaction(
+    currentTrans : Int,
+    cardType: String = "VK Pay",
+    prevTransSum: Int = 0
+) : Int {
+    val comission : Int = when(cardType){
+        "VK Pay" ->  0
+        "Visa", "МИР" -> if (currentTrans*0.0075 < 3500) 3500 else (currentTrans*0.0075).toInt()
+        "Mastercard", "Maestro" -> calcComissionMM(prevTransSum, currentTrans)
+        else -> -1
+    }
+    return comission
+}
+
+fun calcComissionMM (prevTransSum: Int, currentTrans: Int) : Int{
+    val comission :Int = when (val transSum : Int = prevTransSum + currentTrans){
+        in 0..7500000 -> 0
+        else -> if (prevTransSum>7500000) (2000 + 0.006*currentTrans).toInt() else (2000 + 0.006*(transSum-7500000)).toInt()
+    }
+    return comission
+}
---
+++ src/test/kotlin/hw211/MainKtTest.kt
@@ -0,0 +1,141 @@
+package hw211
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun transaction_VK_Pay() {
+        val currentTrans = 10000
+        val cardType = "VK Pay"
+        val prevTransSum = 0
+
+        val result = transaction(
+            currentTrans = currentTrans,
+            cardType = cardType,
+            prevTransSum = prevTransSum
+        )
+
+        assertEquals(0,result)
+    }
+
+    @Test
+    fun transaction_Maestro() {
+        val currentTrans = 1000000
+        val cardType = "Maestro"
+        val prevTransSum = 1000000
+
+        val result = transaction(
+            currentTrans = currentTrans,
+            cardType = cardType,
+            prevTransSum = prevTransSum
+        )
+
+        assertEquals(0,result)
+    }
+
+    @Test
+    fun transaction_Maestro_Overlimits() {
+        val currentTrans = 10000000
+        val cardType = "Maestro"
+        val prevTransSum = 1000000
+
+        val result = transaction(
+            currentTrans = currentTrans,
+            cardType = cardType,
+            prevTransSum = prevTransSum
+        )
+
+        assertEquals(23000,result)
+    }
+
+    @Test
+    fun transaction_Visa() {
+        val currentTrans = 4000000
+        val cardType = "Visa"
+        val prevTransSum = 1000000
+
+        val result = transaction(
+            currentTrans = currentTrans,
+            cardType = cardType,
+            prevTransSum = prevTransSum
+        )
+
+        assertEquals(30000,result)
+    }
+
+    @Test
+    fun transaction_MYR_MinimalComission() {
+        val currentTrans = 40000
+        val cardType = "МИР"
+        val prevTransSum = 1000000
+
+        val result = transaction(
+            currentTrans = currentTrans,
+            cardType = cardType,
+            prevTransSum = prevTransSum
+        )
+
+        assertEquals(3500,result)
+    }
+
+    @Test
+    fun transaction_Incorrect_Card() {
+        val currentTrans = 40000
+        val cardType = "incorrect"
+        val prevTransSum = 1000000
+
+        val result = transaction(
+            currentTrans = currentTrans,
+            cardType = cardType,
+            prevTransSum = prevTransSum
+        )
+
+        assertEquals(-1,result)
+    }
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun calcComissionMM_Prev_Over7500000() {
+        val prevTransSum = 7500001
+        val currentTrans = 10000
+
+        val result = calcComissionMM(
+            prevTransSum = prevTransSum,
+            currentTrans = currentTrans
+        )
+
+        assertEquals(2060,result)
+    }
+
+    @Test
+    fun calcComissionMM_minimalcomissin_firstUplimit() {
+        val prevTransSum = 1000000
+        val currentTrans = 6500001
+
+        val result = calcComissionMM(
+            prevTransSum = prevTransSum,
+            currentTrans = currentTrans
+        )
+
+        assertEquals(2000,result)
+    }
+
+    @Test
+    fun calcComissionMM_no_comission() {
+        val prevTransSum = 0
+        val currentTrans= 10000
+
+        val result = calcComissionMM(
+            prevTransSum = prevTransSum,
+            currentTrans = currentTrans
+        )
+
+        assertEquals(0,result)
+    }
+}
